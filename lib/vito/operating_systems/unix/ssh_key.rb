a7f80f052e2c53713c1d4634cfa9f6ece95b82d4
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-07-30 15:31:13 -0300

    Draft about SSH Key generation

---
+++ lib/vito/operating_systems/unix/ssh_key.rb
@@ -0,0 +1,17 @@
+module Vito
+  module OperatingSystems
+    module Unix
+      class SshKey
+        # First, make sure neither id_rsa.pub nor id_dsa.pub exists.
+        #
+        # Then move on to generate keys:
+        #
+        # $ ssh-keygen -t rsa -C "your_email"
+        #
+        # Deal with questions in the screen
+        #
+        # Output the newly generated SSH Key
+      end
+    end
+  end
+end
