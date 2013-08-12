b998ee83912f8014a862bb55c4d8d808f2e98428
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-08-12 00:49:33 -0300

    Adds Ubuntu10 and Ubuntu12 tests for Rbenv and Ruby

--- spec/acceptance/recipes/ruby_acceptance_spec.rb
+++ spec/acceptance/recipes/ruby_acceptance_spec.rb
@@ -1,24 +1,24 @@
 require "spec_helper"
 
 describe "Ruby recipe" do
-  before do
-    setup_vagrant
-  end
+  Vito::Tests::VagrantTestBox.boxes(:ubuntu10, :ubuntu12).each do |box|
+    it "tests on #{box.name}" do
+      setup_vagrant(box)
 
-  it "installs Git, Rbenv and then Ruby" do
-    assert_installation("git --version").should be_false
-    assert_installation("rbenv --version").should be_false
-    assert_installation("ruby -v").should be_false
+      assert_installation(box, "git --version").should be_false
+      assert_installation(box, "rbenv --version").should be_false
+      assert_installation(box, "ruby -v").should be_false
 
-    Vito::DslFile.new.run do
-      server do
-        connection :ssh, command: 'ssh -i ~/.vagrant.d/insecure_private_key vagrant@localhost -p2222', silent: true
-        install :ruby, version: "1.9.3-p125"
+      Vito::DslFile.new.run do
+        server do
+          connection :ssh, command: "ssh -i ~/.vagrant.d/insecure_private_key vagrant@localhost -p#{box.ssh_port}", verbose: true
+          install :ruby, version: "1.9.3-p125"
+        end
       end
-    end
 
-    assert_installation("git --version").should be_true
-    assert_installation("rbenv --version").should be_true
-    assert_installation("ruby -v").should be_true
+      assert_installation(box, "git --version").should be_true
+      assert_installation(box, "rbenv --version").should be_true
+      assert_installation(box, "ruby -v").should be_true
+    end
   end
 end
