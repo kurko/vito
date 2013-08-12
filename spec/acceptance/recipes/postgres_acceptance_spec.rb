058c33e713ba0f5972dbc4d4f07ea580f97c2f40
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-08-12 10:55:28 -0300

    Adds Ubuntu10 and 12 tests for Postgres

--- spec/acceptance/recipes/postgres_acceptance_spec.rb
+++ spec/acceptance/recipes/postgres_acceptance_spec.rb
@@ -1,20 +1,20 @@
 require "spec_helper"
 
 describe "Postgres recipe" do
-  before do
-    setup_vagrant
-  end
+  Vito::Tests::VagrantTestBox.boxes(:ubuntu10, :ubuntu12).each do |box|
+    it "tests on #{box.name}" do
+      setup_vagrant(box)
 
-  it "installs Postgres" do
-    assert_installation("psql --version").should be_false
+      assert_installation(box, "psql --version").should be_false
 
-    Vito::DslFile.new.run do
-      server do
-        connection :ssh, command: 'ssh -i ~/.vagrant.d/insecure_private_key vagrant@localhost -p2222', silent: true
-        install :postgres, username: 'sebasoga', password: 'kicks_ass'
+      Vito::DslFile.new.run do
+        server do
+          connection :ssh, command: "ssh -i ~/.vagrant.d/insecure_private_key vagrant@localhost -p#{box.ssh_port}", verbose: true
+          install :postgres, username: 'sebasoga', password: 'kicks_ass'
+        end
       end
-    end
 
-    assert_installation("psql --version").should be_true
+      assert_installation(box, "psql --version").should be_true
+    end
   end
 end
